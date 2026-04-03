ace1757fddc84aff78c1272baab9792fa3d95536
Ayo-tomiwa <oyindml1@gmail.com>
2026-04-03 13:38:42 +0100

    feat: added allowjs to tsconfig.json file

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,15 @@
-import { Icon } from "@/components/core";
 import { FC } from "react";
+import WhoWeAre from "@/components/WhoWeAre/whoweare";
+import WhatWeDo from "@/components/WhatWeDo/whatwedo";
+import Blogs from "@/components/Blogs/blogs";
 
 const Home: FC = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="flex flex-col items-center space-y-5">
-        <Icon name="Home" className="text-4xl text-primary" />
-        <h1 className="text-primary font-bold text-4xl">Home</h1>
-        <p className="text-gray-500 text-lg">Welcome to the EAS Platform</p>
-      </div>
-    </div>
+    <>
+      <WhoWeAre />
+      <WhatWeDo />
+      <Blogs />
+    </>
   );
 };
 
